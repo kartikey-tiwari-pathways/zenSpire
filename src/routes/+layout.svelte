20f0abb9bd084ddcf8c6ea28cc3aba8657c3408b
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-03-28 19:05:54 +0530

    Fixed glitch regarding image overlapping child elements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,10 @@
 
 <div class="flex">
 	<Navbar />
-	<div class="bg-[#B19CD9] w-[calc(100vw-80px)] h-screen absolute top-0 right-0 -z-20">
-		<img src="https://crossmagri.com/wp-content/uploads/2021/03/gran-lago-neopop.jpg" alt="bg" class="absolute top-0 left-0 w-full h-full opacity-20 -z-10" />
-		{@render children()}
+	<div class="bg-[#B19CD9] w-[calc(100vw-80px)] h-screen absolute top-0 right-0">
+		<img src="https://crossmagri.com/wp-content/uploads/2021/03/gran-lago-neopop.jpg" alt="bg" class="absolute top-0 left-0 w-full h-full opacity-20 pointer-events-none" />
+		<div class="z-10 opacity-100 backdrop-opacity-100">
+			{@render children()}
+		</div>
 	</div>
 </div>
