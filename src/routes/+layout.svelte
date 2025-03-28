e37ba67ab7d92c3eeb1d54d6eeca7bcf606276d8
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-03-28 19:58:34 +0530

    Made navbar slightly larger; added spacing to separate body and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,9 @@
 
 <div class="flex">
 	<Navbar />
-	<div class="bg-[#B19CD9] w-[calc(100vw-80px)] h-screen absolute top-0 right-0">
+	<div class="bg-[#B19CD9] w-[calc(100vw-96px)] h-screen absolute top-0 right-0">
 		<img src="https://crossmagri.com/wp-content/uploads/2021/03/gran-lago-neopop.jpg" alt="bg" class="absolute top-0 left-0 w-full h-full opacity-20 pointer-events-none" />
-		<div class="z-10 opacity-100 backdrop-opacity-100">
+		<div class="ml-4 z-10 opacity-100 backdrop-opacity-100">
 			{@render children()}
 		</div>
 	</div>
