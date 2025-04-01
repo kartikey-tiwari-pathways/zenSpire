93b2b2145a61a2b5c6250e99ff97a24df7cb6dd4
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-04-01 15:12:13 +0530

    Offensive tester image replaced

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from "$app/state";
+    console.log ()
+</script>
+<div class="flex flex-col justify-center items-center mt-2">
+    {#if page.status === 404}
+    <img src="https://media.tenor.com/IHdlTRsmcS4AAAAM/404.gif" alt="404 gif">
+    {:else}
+        <p class="text-4xl text-white mt-8 font-bold">{page.status}</p>
+    {/if}
+    <p class="text-4xl text-white mt-8 font-bold">{page.error.message}</p>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <div class="flex">
 	<Navbar />
 	<div class="bg-[#B19CD9] w-[calc(100vw-96px)] h-screen absolute top-0 right-0">
-		<img src="https://crossmagri.com/wp-content/uploads/2021/03/gran-lago-neopop.jpg" alt="bg" class="absolute top-0 left-0 w-full h-full opacity-20 pointer-events-none" />
+		<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgL0Ujmwvp_WdxaxVDIq3yyTHWHJ7-702wXjpI1D8RPOvWdsJV35Z0oW_ZtfUCUAX-IdDQo5L3FZugGtDTz8-Q-SFflUxXzMOlDZkH5NgkpcezUegU1rkz0ZTgHCKfnuG7SrUT1jSzs2Cw/s1600/Neopop_daisies.jpg" alt="bg" class="absolute top-0 left-0 w-full h-full opacity-20 pointer-events-none" />
 		<div class="ml-4 z-10 opacity-100 backdrop-opacity-100">
 			{@render children()}
 		</div>
