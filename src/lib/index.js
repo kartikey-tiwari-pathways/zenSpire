ab8ee24f81cba7e4c1f22e4add5b5ad766306509
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-04-02 18:48:10 +0530

    Added helper functions for checking current authentication status

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,39 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { onAuthStateChanged } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmp1Akf_2ONZt-oQQkmGEEeTgv0Fl7FJQ",
+    authDomain: "zenspire-a9e65.firebaseapp.com",
+    projectId: "zenspire-a9e65",
+    storageBucket: "zenspire-a9e65.firebasestorage.app",
+    messagingSenderId: "207132632281",
+    appId: "1:207132632281:web:b8e7895dfab27ccc77cac8"
+};
+
+// Functions
+
+function currentUser(auth) {
+    onAuthStateChanged(auth, user => {
+        return user;
+    });
+}
+
+/**
+ * Gets user data
+ * @returns {Object} Returns uid, displayName, email, photoURL, emailVerified
+ */
+function getUserData(auth) {
+    onAuthStateChanged(auth, user => {
+        return {
+            uid: user.uid,
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+            emailVerified: user.emailVerified
+        };
+    });
+}
+
+export { currentUser, firebaseConfig }
