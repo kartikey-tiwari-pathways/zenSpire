0b0100e36abcf47f66e30e37f2d0d41a9ee4532c
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-04-02 18:54:57 +0530

    Fixed issue with form submission not resulting in any action

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -15,7 +15,7 @@
     let resultElement;
     let resultElementBase = "text-2xl font-bold mt-2 border-2 bg-black p-2";
 
-    function signUp() {
+    function login() {
         signInWithEmailAndPassword(auth, email, password).then(() => {
             resultElement.className = `${resultElementBase} text-green-500`;
             resultElement.textContent = "Success!";
@@ -32,7 +32,7 @@
 
 <div class="flex flex-col items-center justify-center w-[calc(100vw-96px)] h-screen">
     <p class="text-4xl text-white font-bold">Welcome back!</p>
-    <form class="flex flex-col items-center mt-6">
+    <form class="flex flex-col items-center mt-6" on:submit={login}>
         <input autocomplete="off" bind:value={email} required type="text" name="email" id="email" class="w-auto h-10 rounded-md bg-white text-black text-base p-4 m-4 border-2" placeholder="Enter your email...">
         <input autocomplete="off" bind:value={password} required type="password" name="password" id="password" class="w-auto h-10 rounded-md bg-white text-black text-base p-4 border-2" placeholder="Enter your password...">
         <button class="w-fit h-10 bg-white rounded-2xl py-3 text-2xl flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-400 transition-all mt-8 p-4 border-2">Sign up</button>
