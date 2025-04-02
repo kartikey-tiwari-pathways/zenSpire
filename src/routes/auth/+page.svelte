0089d50d3af9b68142889603dc35b602fb60de8b
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-04-02 18:48:39 +0530

    Started work on authentication (basic framework finished)

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { currentUser, firebaseConfig } from "$lib";
+    import { initializeApp } from "firebase/app";
+    import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+    import { onMount } from "svelte";
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+
+    let resultElement;
+    let resultElementBase = "text-2xl font-bold mt-2 border-2 bg-black p-2";
+
+    onMount(() => {
+        if (currentUser(auth)) {
+            goto("/");
+        }
+    })
+
+    function newAccount() {
+        goto("/auth/signup");
+    }
+
+    function login() {
+        goto("/auth/login");
+    }
+
+    function google() {
+        signInWithPopup(auth, googleProvider).then(() => {
+            resultElement.className = `${resultElementBase} text-green-500`;
+            resultElement.textContent = "Success!";
+        }).catch(err => {
+            resultElement.className = `${resultElementBase} text-red-500`;
+            resultElement.textContent = `Error: ${err.code}`;
+        });
+    }
+</script>
+
+<div class="flex flex-col items-center justify-center w-[calc(100vw-96px)] h-screen">
+    <p class="text-4xl font-bold text-black">Welcome!</p>
+    <button on:click={newAccount} class="w-auto min-w-72 h-auto min-h-16 bg-white rounded-2xl p-2 text-2xl flex justify-center items-center cursor-pointer hover:bg-gray-400 transition-all mt-8">👋 Create an account</button>
+    <button on:click={login} class="w-auto min-w-80 h-auto min-h-16 bg-white rounded-2xl py-3 text-2xl flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-400 transition-all mt-8"><p>🤗</p><p>Already have an account?</p></button>
+    <button on:click={google} class="w-auto min-w-80 h-auto min-h-16 bg-white rounded-2xl py-3 text-2xl flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-400 transition-all mt-8"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png" alt="google_logo" class="w-6 h-6"><p>Continue with Google</p></button>
+    <p bind:this={resultElement} class="invisible">How can you see this?</p>
+</div>
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { firebaseConfig } from "$lib";
+    import { initializeApp } from "firebase/app";
+    import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    let email = "";
+    let password = "";
+    /**
+     * @type Document
+     */
+    let resultElement;
+    let resultElementBase = "text-2xl font-bold mt-2 border-2 bg-black p-2";
+
+    function signUp() {
+        signInWithEmailAndPassword(auth, email, password).then(() => {
+            resultElement.className = `${resultElementBase} text-green-500`;
+            resultElement.textContent = "Success!";
+        }).catch(err => {
+            resultElement.className = `${resultElementBase} text-red-500`;
+            resultElement.textContent = `Error: ${err.code}`;
+        });
+    }
+
+    function back() {
+        goto("../");
+    }
+</script>
+
+<div class="flex flex-col items-center justify-center w-[calc(100vw-96px)] h-screen">
+    <p class="text-4xl text-white font-bold">Welcome back!</p>
+    <form class="flex flex-col items-center mt-6">
+        <input autocomplete="off" bind:value={email} required type="text" name="email" id="email" class="w-auto h-10 rounded-md bg-white text-black text-base p-4 m-4 border-2" placeholder="Enter your email...">
+        <input autocomplete="off" bind:value={password} required type="password" name="password" id="password" class="w-auto h-10 rounded-md bg-white text-black text-base p-4 border-2" placeholder="Enter your password...">
+        <button class="w-fit h-10 bg-white rounded-2xl py-3 text-2xl flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-400 transition-all mt-8 p-4 border-2">Sign up</button>
+        <p bind:this={resultElement} class="invisible">How can you see this?</p>
+    </form>
+    <button on:click={back} class="w-fit h-10 bg-white rounded-2xl py-3 text-2xl flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-400 transition-all mt-8 p-4 border-2">Back</button>
+</div>
