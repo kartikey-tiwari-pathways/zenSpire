5e87fd3ac3a21a84be404b3ac3793e5dab356cf7
kartikey-tiwari-pathways <kartikey.tiwari@pathwaysschool.in>
2025-04-02 18:51:41 +0530

    Fixed issue with website logo not rendering on first navbar widget

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div id="navbar" class="h-screen w-24 bg-[#B13DFA] flex items-center justify-center flex-col">
-    <SidebarWidgets emoji='<img src="favicon.png" class="w-6 h-6" />'>zenSpire</SidebarWidgets>
+    <SidebarWidgets emoji='<img src="/favicon.png" class="w-6 h-6" />'>zenSpire</SidebarWidgets>
     <SidebarWidgets emoji="🏠" interactive="/">Home</SidebarWidgets>
     <SidebarWidgets emoji="💬" interactive="/chat">Chat</SidebarWidgets>
     <SidebarWidgets emoji="📖" interactive="/journal">Journal</SidebarWidgets>
